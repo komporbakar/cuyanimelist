5baf8625732fca2f7d1ed82c4bc6ce6584cedf3c
komporbakar <laravelgg00@gmail.com>
2023-12-03 19:28:54 +0700

    add handling error

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -9,39 +9,39 @@ const Page = async ({ params: { id } }) => {
     <>
       <div className="pt-4 px-4">
         <h3 className="text-2xl text-color-primary">
-          {data.title} - {data.year}
+          {data?.title} - {data?.year}
         </h3>
       </div>
       <div className="pt-4 px-4 flex gap-2 text-color-primary overflow-x-auto">
         <div className="w-36 flex flex-col items-center rounded border p-2">
           <h3>Peringkat</h3>
-          <p>{data.rank}</p>
+          <p>{data?.rank}</p>
         </div>
         <div className="w-36 flex flex-col items-center rounded border p-2">
           <h3>Score</h3>
-          <p>{data.score}</p>
+          <p>{data?.score}</p>
         </div>
         <div className="w-36 flex flex-col items-center rounded border p-2">
           <h3>Members</h3>
-          <p>{data.members}</p>
+          <p>{data?.members}</p>
         </div>
         <div className="w-36 flex flex-col items-center rounded border p-2">
           <h3>Episodes</h3>
-          <p>{data.episodes}</p>
+          <p>{data?.episodes}</p>
         </div>
       </div>
       <div className="pt-4 px-4 gap-2 flex sm:flex-nowrap flex-wrap text-color-primary">
         <Image
-          src={data.images.webp.image_url}
-          alt={data.images.jpg.image_url}
+          src={data?.images.webp.image_url}
+          alt={data?.images.jpg.image_url}
           width={250}
           height={250}
           className="w-full rounded object-cover"
         />
-        <p className=" mt-5 text-lg">{data.synopsis}</p>
+        <p className=" mt-5 text-lg">{data?.synopsis}</p>
       </div>
       <div>
-        <VideoPlayer youtubeId={data.trailer.youtube_id} />
+        <VideoPlayer youtubeId={data?.trailer.youtube_id} />
       </div>
     </>
   );
